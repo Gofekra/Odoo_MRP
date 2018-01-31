7ef9497f2b49bd5548bdba3afed78b2456b46188
Ivan Porras <ivanporraslopez@gmail.com>
2018-01-31 17:27:52 -0600

    Add files via upload

--- update_product_standard_price_propagation.sql
+++ update_product_standard_price_propagation.sql
@@ -1,26 +1,27 @@
 ﻿/**
   @author: ivan_porras
-  @description: 
+  @description: Actualiza el costo estandar de un producto
 */
-CREATE OR REPLACE FUNCTION parse_trigger_updatestandardprice_for() RETURNS trigger AS $$
+--- T
+CREATE OR REPLACE FUNCTION parse_trigger_updatestandardprice_for()  RETURNS trigger AS $$
 DECLARE
   r record;
   c CURSOR FOR select  pt.id as idpt,pp.id as idpp,mrp.total_cost from product_template pt inner join product_product pp on pt.default_code = pp.default_code
-	        inner join mrp_bom mrp on mrp.product_tmpl_id = pt.id;
+          inner join mrp_bom mrp on mrp.product_tmpl_id = pt.id;
 BEGIN
   FOR r IN c LOOP
                 IF r.total_cost <> (select value_float from ir_property where name = 'standard_price' and (res_id = 'product.product,' || cast(r.idpt as varchar) or res_id = 'product.product,' || cast(r.idpp as varchar)) limit 1)
-	THEN
-		update ir_property set value_float = r.total_cost where name = 'standard_price' 
-		and (res_id = 'product.product,' || cast(r.idpt as varchar) or res_id = 'product.product,' || cast(r.idpp as varchar));
-	END IF;
+  THEN
+    update ir_property set value_float = r.total_cost where name = 'standard_price' 
+    and (res_id = 'product.product,' || cast(r.idpt as varchar) or res_id = 'product.product,' || cast(r.idpp as varchar));
+  END IF;
   END LOOP;    
   RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+
 
 /**
-    @author: ivan_porras
+    trigger para executar parse_trigger_updatestandarprice_for
 */
 CREATE TRIGGER check_update
     BEFORE UPDATE OF total_cost ON mrp_bom
@@ -30,22 +31,45 @@ CREATE TRIGGER check_update
 
 
 
-CREATE OR REPLACE FUNCTION updatestandardprice() RETURNS void AS $$
+/*
+  Actualizar manualmente el precio estandar de todos los productos que tienen listas de materiales
+*/
+
+CREATE OR REPLACE FUNCTION updatestandardprice()  RETURNS void AS $$
 DECLARE
   r record;
   c CURSOR FOR select  pt.id as idpt,pp.id as idpp,mrp.total_cost from product_template pt inner join product_product pp on pt.default_code = pp.default_code
-	        inner join mrp_bom mrp on mrp.product_tmpl_id = pt.id;
+          inner join mrp_bom mrp on mrp.product_tmpl_id = pt.id;
 BEGIN
   FOR r IN c LOOP
                 IF r.total_cost <> (select value_float from ir_property where name = 'standard_price' and (res_id = 'product.product,' || cast(r.idpt as varchar) or res_id = 'product.product,' || cast(r.idpp as varchar)) limit 1)
-	THEN
-		update ir_property set value_float = r.total_cost where name = 'standard_price' 
-		and (res_id = 'product.product,' || cast(r.idpt as varchar) or res_id = 'product.product,' || cast(r.idpp as varchar));
-	END IF;
+  THEN
+    update ir_property set value_float = r.total_cost where name = 'standard_price' 
+    and (res_id = 'product.product,' || cast(r.idpt as varchar) or res_id = 'product.product,' || cast(r.idpp as varchar));
+  END IF;
+  END LOOP;  
+END;
+$$ LANGUAGE plpgsql;
+
+
+/*
+  Inserta precio estandar para nuevos productos con listas de materiales
+*/
+CREATE OR REPLACE FUNCTION insertStandardPrice() RETURNS void AS $$
+DECLARE
+  r record;
+  c CURSOR FOR select  pt.id as idpt,pp.id as idpp,mrp.total_cost from product_template pt inner join product_product pp on pt.default_code = pp.default_code
+          inner join mrp_bom mrp on mrp.product_tmpl_id = pt.id;
+BEGIN
+  FOR r IN c LOOP
+        IF (select count(name) from ir_property where name = 'standard_price' and res_id = 'product.product,' || r.idpp limit 1)  = 0
+  THEN
+      INSERT INTO ir_property(create_uid, value_float, name,res_id, company_id, fields_id, type)
+      VALUES (1, 1.0, 'standard_price', 'product.product,' || r.idpp, 1, 2102,'float');     
+  END IF;
   END LOOP;  
 END;
 $$ LANGUAGE plpgsql;
 
 
 
-select * from updatestandardprice()
